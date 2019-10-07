87164e16d37e352526f7ce055255f12461fcbf25
mr.Blue <yashinsergey125@gmail.com>
2019-10-07 20:18:53 +0300

    added onBackPressed()

--- app/src/main/java/com/example/newyorktimes/MainActivity.java
+++ app/src/main/java/com/example/newyorktimes/MainActivity.java
@@ -1,9 +1,10 @@
 package com.example.newyorktimes;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.webkit.WebView;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.newyorktimes.network.NewsModel;
 
@@ -17,16 +18,19 @@ public class MainActivity extends AppCompatActivity {
 
     private WebView webView;
 
+    @SuppressLint("SetJavaScriptEnabled")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         webView = findViewById(R.id.webView);
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.setWebViewClient(new MyWebViewClient());
 
-        updateWeatherData();
+        updateData();
     }
 
-    private void updateWeatherData() {
+    private void updateData() {
         NetworkService.getNetworkService().getAPI().loadNews(getString(R.string.api_key)).retry(3)
                 .subscribeOn(Schedulers.io())
                 .observeOn(AndroidSchedulers.mainThread())
@@ -49,5 +53,14 @@ public class MainActivity extends AppCompatActivity {
     private void renderData(NewsModel model) {
        webView.loadUrl(model.getResults().get(0).getUrl());
 
+    }
+
+    @Override
+    public void onBackPressed() {
+        if(webView.canGoBack()) {
+            webView.goBack();
+        } else {
+            super.onBackPressed();
+        }
     }
 }
